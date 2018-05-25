525b9e911abef05061d3ac5ad5f8be490fd1b51e
James Poole <james.poole@bjss.com>
2018-05-25 15:19:19 +0100

    Added gitignore

--- Section3_Fundamental/src/computrade/enumsample/Rank.java
+++
@@ -1,7 +0,0 @@
-package computrade.enumsample;
-
-public enum Rank {
-	
-	DEUCE, THREE, FOUR, FIVE, SIX, SEVEN,
-	EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE 
-}
--- Section3_Fundamental/src/computrade/string/Other.java
+++
@@ -1,7 +0,0 @@
-package computrade.string;
-
-public class Other {
-
-		static String stringFromOtherClass = "myTestString"; 
-		
-}
